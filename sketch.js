5380178e12244cb00a6a8ba21623bfe39dea3d6b
web-extraordinaire <127468383+web-extraordinaire@users.noreply.github.com>
2023-03-23 13:48:02 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 let x = 0;
-
+let line = 200
 function setup() {
-  background(500);
+  background(100);
 }
 
 function draw() {
+  line(y, 25, y, 90);
   ellipse(x, height/2, 20, 20);
+  rect(x,y,50,50);
   x = x + 1;
+  y = y + 2;
 }
