fc6c52fa4f473e552f48a401c4df693882275c48
web-extraordinaire <127468383+web-extraordinaire@users.noreply.github.com>
2023-03-30 09:29:57 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,10 +1,10 @@
-function drawMediumPerson(x) {
+function drawMediumPerson(x,word) {
   ellipse(x,100,100,100);
   line(x,150,x,300);
   line(x+20,150,x,200);
   line(x-20,150,x,200);
   line(x+20,350,x,300);
   line(x-20,350,x,300);
-    text("desserts",x,350)
+    text(word,x,350)
     x = x + 1;
 }
