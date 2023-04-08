24111c2fd23445a072b50d6b1c9c749ab1129e01
web-extraordinaire <127468383+web-extraordinaire@users.noreply.github.com>
2023-04-08 15:28:11 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -8,3 +8,13 @@ function drawMediumPerson(x,y,word) {
     text(word,x,y-90)
     
 }
+function drawSmallPerson(x,y,word) {
+     ellipse(x+50,y-50,26,26);
+        line(x+50,y-10,x+50,y-37);
+        line(x+40,y-35,x+50,y-27);
+        line(x+60,y-35,x+50,y-27);
+        line(x+60,y,x+50,y-12);
+        line(x+40,y,x+50,y-12);
+  text(word,x y+ 30);
+
+}
